7373be662b1de7b56eab0447a290b237f29baae4
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-01-24 08:28:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (18.58%)

---
+++ 0027-remove-element/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        result = []
+        for i in nums:
+            if (i != val):
+                result.append(i)
+
+        nums[:] = result
+        return len(nums)
+        

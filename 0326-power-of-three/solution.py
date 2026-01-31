cfff23dc58afa7231b7e041ac65c50fc7cf404f5
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-01-31 07:13:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (61.49%)

---
+++ 0326-power-of-three/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n >= 1:
+            while n > 1:
+                if n%3 == 0:
+                    n = (n / 3)
+                else:
+                    return False
+            return True
+        return False

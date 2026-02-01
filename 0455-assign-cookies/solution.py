4c9859c8e61c85c163c2ab5fb9b71caff8221936
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-02-01 09:42:40 +0000

    Sync LeetCode submission Runtime - 22 ms (62.02%), Memory - 21.8 MB (87.23%)

---
+++ 0455-assign-cookies/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        count = 0
+        i = len(g) - 1
+        j = len(s) - 1
+
+        while i >= 0 and j >= 0:
+            if s[j] >= g[i]:
+                count += 1
+                j -= 1
+            i -= 1
+        return count

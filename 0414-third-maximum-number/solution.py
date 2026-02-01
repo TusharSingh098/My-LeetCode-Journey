8247c9806d07de8c41cbd1aaa787dd85787a7014
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-02-01 06:13:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.6 MB (8.84%)

---
+++ 0414-third-maximum-number/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        if (len(nums_set) < 3):
+            return max(nums_set)
+        for _ in range(2):
+            nums_set.remove(max(nums_set))
+        return max(nums_set)
+
+        

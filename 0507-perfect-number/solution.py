34104b49dcea25affb9d3b2f329afd947e06e2e8
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-02-02 15:33:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (14.96%)

---
+++ 0507-perfect-number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if (num <= 0) or (num % 10 != 6) and (num % 100 != 28):
+            return False
+
+        sum = 1
+        for i in range(2, int(num ** 0.5) + 1):
+            if (num % i == 0):
+                sum += i
+                if (i * i != num):
+                    sum += num / i
+        return sum == num

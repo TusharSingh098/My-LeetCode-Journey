ca838fb51f503df51686c1b6b4b3e58083e0f29e
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-02-06 18:44:53 +0000

    Sync LeetCode submission Runtime - 49 ms (10.31%), Memory - 25.5 MB (35.70%)

---
+++ 0789-kth-largest-element-in-a-stream/solution.py
@@ -0,0 +1,12 @@
+import bisect
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = sorted(nums)
+
+    def add(self, val: int) -> int:
+        bisect.insort(self.nums, val)
+        
+        return self.nums[-self.k]
+

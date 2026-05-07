cbe530f90a3655ad050dea91e274b249489382a5
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-05-07 09:29:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.3 MB (86.83%)

---
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice{INT_MAX}, maxProfit{0};
+        for (const int& price: prices)
+        {
+            if (price < minPrice)
+            {
+                minPrice = price;
+            }
+            else if (price - minPrice > maxProfit)
+            {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+};

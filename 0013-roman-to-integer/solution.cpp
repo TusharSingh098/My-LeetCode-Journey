fbcaca9ee4add7625763aafa8a7155fad368b596
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-05-15 16:35:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (86.76%)

---
+++ 0013-roman-to-integer/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    inline int val(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    int romanToInt(string s) {
+        int sum{0}, prev{0};
+
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            int curr{val(s[i])};
+            if (curr < prev) sum -= curr;
+            else sum += curr;
+            prev = curr;
+        }
+        return sum;
+    }
+};

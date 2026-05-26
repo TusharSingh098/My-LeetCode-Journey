272843355fec6e51536636522043c945d3b276e4
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-05-26 18:18:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (70.12%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,16 +1,24 @@
 class Solution {
+private:
+    double solve(double x, long long power)
+    {
+        if (power == 0) return 1.0;
+
+        double half{solve(x, power / 2)};
+
+        if (power % 2 == 0) return half * half;
+        else return half * half * x;
+    }
+
 public:
     double myPow(double x, int n) {
         double ans{1.0};
-        long long power = n;
-        if (power < 0) power = -power;
-        
-        while (power > 0)
+        long long power{n};
+        if (power < 0) 
         {
-            if (power % 2 == 1) ans *= x;
-            power /= 2;
-            x *= x;
+            x = 1 / x;
+            power = -power;
         }
-        return n >= 0 ? ans : 1 / ans;
+        return solve(x, power);
     }
 };

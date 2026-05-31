4b315b5afaa56a577da44b0602ee261ffc0ccfdb
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-05-31 13:15:50 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -9,8 +9,8 @@ private:
         char letter{board[row][col]};
         board[row][col] = '*';
 
-        if (row > 0 && board[row - 1][col] == word[idx + 1] && solve(board, word, row - 1, col, idx + 1)) return true;;
-        if (row < board.size() - 1 && board[row + 1][col] == word[idx + 1] && solve(board, word, row + 1, col, idx + 1)) return true;;
+        if (row > 0 && board[row - 1][col] == word[idx + 1] && solve(board, word, row - 1, col, idx + 1)) return true;
+        if (row < board.size() - 1 && board[row + 1][col] == word[idx + 1] && solve(board, word, row + 1, col, idx + 1)) return true;
         if (col > 0 && board[row][col - 1] == word[idx + 1] && solve(board, word, row, col - 1, idx + 1)) return true;
         if (col < board[0].size() - 1 && board[row][col + 1] == word[idx + 1] && solve(board, word, row, col + 1, idx + 1)) return true;
 
@@ -22,11 +22,23 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int row = board.size(), col = board[0].size();
 
+        int bFreq[128] = {0}, wFreq[128] = {0};
+        for (int i{0}; i < row; i++) for (int j{0}; j < col; j++) bFreq[board[i][j]]++;
+        for (char ch: word) wFreq[ch]++;
+
+        for (int i{65}; i < 123; i++) if (bFreq[i] < wFreq[i]) return false;
+
+        string target{word};
+        if (bFreq[word.back()] < bFreq[word[0]]) 
+        {
+            reverse(target.begin(), target.end());
+        }
+
         for (int i{0}; i < row; i++)
         {
             for (int j{0}; j < col; j++)
             {
-                if (solve(board, word, i, j, 0)) return true;
+                if (solve(board, target, i, j, 0)) return true;
             }
         }
         return false;

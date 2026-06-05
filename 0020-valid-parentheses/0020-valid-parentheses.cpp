9260b38623697e884325d9b60b66ceec7cb11d90
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-05 22:48:11 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isValid(string s) {
+        if (s.length() % 2 != 0) return false;
+        
         unordered_map<char, char> mpp{{')','('},{'}','{'},{']','['}};
         stack<char> stk;
         for (char ch: s)

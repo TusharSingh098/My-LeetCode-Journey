c818b9945d4a7f625f222323d8a9ddb1de0d2e19
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-08 23:10:10 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,55 +1,42 @@
 class Solution {
-private:
-    struct Boundaries 
-    {
-        pair<int, int> minBounds;
-        pair<int, int> maxBounds;
-    };
-
-    vector<Boundaries> solve(vector<int>& nums) 
-    {
-        int n = nums.size();
-        vector<Boundaries> ans(n, {{-1, n}, {-1, n}});
-        stack<int> stk1, stk2;
-
-        for (int i{0}; i < n; i++)
-        {
-            while (!stk1.empty() && nums[stk1.top()] > nums[i]) stk1.pop();
-
-            if (!stk1.empty()) ans[i].minBounds.first = stk1.top();
-            stk1.push(i);
-
-            while (!stk2.empty() && nums[stk2.top()] < nums[i]) stk2.pop();
-
-            if (!stk2.empty()) ans[i].maxBounds.first = stk2.top();
-            stk2.push(i);
-        }
-
-        stk1 = stk2 = {};
-        for (int i{n - 1}; i >= 0; i--)
-        {
-            while (!stk1.empty() && nums[stk1.top()] >= nums[i]) stk1.pop();
-
-            if (!stk1.empty()) ans[i].minBounds.second = stk1.top();
-            stk1.push(i);
-
-            while (!stk2.empty() && nums[stk2.top()] <= nums[i]) stk2.pop();
-
-            if (!stk2.empty()) ans[i].maxBounds.second = stk2.top();
-            stk2.push(i);
-        }
-        return ans;
-    }
-
 public:
     long long subArrayRanges(vector<int>& nums) {
-        vector<Boundaries> indices{solve(nums)};
-        long long ans{0};
-        for (int i{0}; i < nums.size(); i++)
+        int n = nums.size();
+        long long ans = 0;
+        
+        vector<int> minStk, maxStk;
+        minStk.reserve(n);
+        maxStk.reserve(n);
+
+        for (int i = 0; i <= n; i++) 
         {
-            long long maxi{1LL * (i - indices[i].maxBounds.first) * (indices[i].maxBounds.second - i)}, mini{1LL * (i - indices[i].minBounds.first) * (indices[i].minBounds.second - i)};
-            ans += (maxi - mini) * nums[i];
+            while (!minStk.empty() && (i == n || nums[minStk.back()] > nums[i])) 
+            {
+                int poppedIdx = minStk.back();
+                minStk.pop_back();
+                
+                int leftBound = minStk.empty() ? -1 : minStk.back();
+                int rightBound = i;
+                
+                long long subarraysAsMin = 1LL * (poppedIdx - leftBound) * (rightBound - poppedIdx);
+                ans -= subarraysAsMin * nums[poppedIdx];
+            }
+            minStk.push_back(i);
+            
+            while (!maxStk.empty() && (i == n || nums[maxStk.back()] < nums[i])) 
+            {
+                int poppedIdx = maxStk.back();
+                maxStk.pop_back();
+                
+                int leftBound = maxStk.empty() ? -1 : maxStk.back();
+                int rightBound = i;
+                
+                long long subarraysAsMax = 1LL * (poppedIdx - leftBound) * (rightBound - poppedIdx);
+                ans += subarraysAsMax * nums[poppedIdx];
+            }
+            maxStk.push_back(i);
         }
+        
         return ans;
     }
 };

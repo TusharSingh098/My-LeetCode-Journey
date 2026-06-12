df29c8d011493c3fdbce50996038b907191b33c7
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-12 23:31:10 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> temp(n, 0);
+        stack<int> stk;
+        int ans{0};
+
+        for (int i{0}; i < m; i++)
+        {
+            stk = {};
+            for (int j{0}; j <= n; j++)
+            {
+                if (j < n) temp[j] = (matrix[i][j] == '1') ? temp[j] + 1 : 0;
+
+                int currVal{(j == n) ? 0 : temp[j]};
+
+                while (!stk.empty() && temp[stk.top()] >= currVal)
+                {
+                    int mid{stk.top()};
+                    stk.pop();
+
+                    int pse{stk.empty() ? -1 : stk.top()}, nse{j};
+
+                    ans = max(ans, (nse - pse - 1) * temp[mid]);
+                }
+                stk.push(j);
+            }
+        }
+        return ans;
+    }
+};

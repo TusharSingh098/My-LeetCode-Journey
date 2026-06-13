9a718ac80ae05ce8733fbbecfc3a58a464c14c05
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-13 22:14:24 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
+        ans.reserve(nums.size() - k + 1);
         deque<int> dq;
 
         for (int i{0}; i < nums.size(); i++)

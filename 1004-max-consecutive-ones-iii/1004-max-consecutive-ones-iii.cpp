cc735bb05c84fde1506e0a8ea6e4735dcf4818f2
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-24 14:52:40 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size();
-        int left{0}, right{0}, numZeros{0}, maxLen{0};
+        int left{0}, numZeros{0}, maxLen{0};
 
-        while (right < n)
+        for (int right{0}; right < nums.size(); right++)
         {
-            if (nums[right] == 0)
+            if (!nums[right]) 
             {
-                if (numZeros < k) numZeros++;
-                else
-                {
-                    while (left < right && nums[left]) left++;
-                    left++;
-                }
+                numZeros++;
             }
-            right++;
-            maxLen = max(maxLen, right - left);          
+
+            while (numZeros > k) 
+            {
+                if (!nums[left]) numZeros--;
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
         }
         return maxLen;
     }

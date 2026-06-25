05bace0ce5762a75eea5aa9ca2fb78a0ba8b1187
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-25 12:35:38 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    int solve(vector<int>& nums, int goal)
+    {
+        if (goal < 0) return 0;
+        int left{0}, sum{0}, count{0};
+
+        for (int right{0}; right < nums.size(); right++)
+        {
+            sum += nums[right];
+
+            while (sum > goal)
+            {
+                sum -= nums[left];
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+        return count;
+    }
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return solve(nums, goal) - solve(nums, goal - 1);
+    }
+};

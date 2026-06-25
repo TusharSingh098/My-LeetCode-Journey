71765f8b4455dd58bb15c2ef5b3b8fbbe878b3ee
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-25 12:07:55 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left{0}, right{0}, maxFreq{0}, maxLen{0};
+        int left{0}, maxFreq{0}, maxLen{0};
         int freqs[26] = {0};
-        for (right = 0; right < s.length(); right++)
+        for (int right{0}; right < s.length(); right++)
         {
             maxFreq = max(maxFreq, ++freqs[s[right] - 'A']);
 
-            while (right - left + 1 - maxFreq > k)
+            if (right - left + 1 - maxFreq > k) 
             {
                 freqs[s[left] - 'A']--;
-                left++;
-                maxLen = 0;
-                for (int freq: freqs) maxLen = max(maxLen, freq);
-            }     
+                left++; 
+            }
 
-            int currLen{right - left + 1};
-            if (currLen - maxFreq <= k) maxLen = max(maxLen, currLen);
+            maxLen = max(maxLen, right - left + 1);
         }
         return maxLen;
     }

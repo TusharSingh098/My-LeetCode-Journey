b7fea17f07aefcfd42a46a0935600561dc7a15e5
Tushar Singh <tusharsinghrtk3008@gmail.com>
2026-06-26 12:20:21 +0530

    [{Difficulty}] [{Problem Topic}] {Problem Name} | Time: {Time} ({Time Percentile}) | Space: {Space} ({Space Percentile}) | Lang: {Solved Language}

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int solve(std::vector<int>& nums, int k)
+    {
+        if (k <= 0) return 0;
+        
+        int left{0}, distincts{0}, count{0};
+        std::unordered_map<int, int> mpp;
+
+        for (int right{0}; right < nums.size(); right++)
+        {
+            if (mpp[nums[right]]++ == 0) distincts++;
+
+            while (distincts > k)
+            {
+                if (mpp[nums[left++]]-- == 1) distincts--;
+            }
+
+            count += right - left + 1;
+            std::cout << right - left + 1 << "\n";
+        }
+
+        return count;
+    }
+
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return solve(nums, k) - solve(nums, k - 1);
+    }
+};
